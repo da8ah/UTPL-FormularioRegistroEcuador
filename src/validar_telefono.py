a82c207b2290f821cb3ebf0988f028b7b18e516e
Scoowy <gahonajuanjo@gmail.com>
2022-02-08 10:47:55 -0500

    Mejora a la validacion de telefono

--- src/validar_telefono.py
+++ src/validar_telefono.py
@@ -3,6 +3,23 @@ import re
 
 def validar_telefono(telefono: str):
     telefono = re.sub(r'[\s\_\-\\/.,\'\"\(\)]', '', telefono)
-    return re.compile(
-        r'([+]\d{3})?\d{9,10}'
-    ).match(telefono) is not None
+    lenghtTel = len(telefono)
+
+    matches = re.compile(r'([+]\d{3})?\d{9,10}').match(telefono)
+
+    # Se comprueba que exista un match
+    if (matches is None):
+        return False
+
+    # Si existe el primer grupo '+000' el numero no debe ser
+    # mayor a 14 digitos
+    if (matches.group(1) and lenghtTel > 14):
+        return False
+
+    # Si no existe el primer grupo '+000' el numero no debe ser
+    # mayor a 10 ni menor a 9
+    if (matches.group(1) is None and (lenghtTel > 10 or lenghtTel < 9)):
+        return False
+
+    # Si pasa todo es valido
+    return True
